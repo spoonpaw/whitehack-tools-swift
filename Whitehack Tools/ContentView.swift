e59cce63137340b12bc71f3cd4f42b5ec518d0fc
zftcce czgqbs <alexanderstclair@zoho.com>
2024-11-15 01:53:02 -0800

    Initial Commit

---
+++ Whitehack Tools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whitehack Tools
+//
+//  Created by System Admin on 11/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whitehack Tools/Whitehack_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whitehack_ToolsApp.swift
+//  Whitehack Tools
+//
+//  Created by System Admin on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Whitehack_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
