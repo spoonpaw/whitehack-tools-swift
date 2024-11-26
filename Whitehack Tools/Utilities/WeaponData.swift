1764923564bd7834a6f0aeb11c973ca53bc40c0b
zftcce czgqbs <alexanderstclair@zoho.com>
2024-11-26 05:47:11 -0800

    WEAPON WORK PT 10

--- Whitehack Tools/Utilities/WeaponData.swift
+++ Whitehack Tools/Utilities/WeaponData.swift
@@ -2,18 +2,27 @@ import Foundation
 
 struct WeaponData {
     static let weapons = [
-        ["name": "Dagger", "damage": "1d6", "weight": "No size", "rateOfFire": "1", "special": ""],
-        ["name": "Short Sword", "damage": "1d6", "weight": "Minor", "rateOfFire": "1", "special": ""],
-        ["name": "Long Sword", "damage": "1d8", "weight": "Regular", "rateOfFire": "1", "special": ""],
-        ["name": "Great Sword", "damage": "1d10", "weight": "Heavy", "rateOfFire": "1", "special": ""],
-        ["name": "Club", "damage": "1d4", "weight": "Minor", "rateOfFire": "1", "special": ""],
-        ["name": "Mace", "damage": "1d6", "weight": "Regular", "rateOfFire": "1", "special": ""],
-        ["name": "War Hammer", "damage": "1d8", "weight": "Regular", "rateOfFire": "1", "special": ""],
-        ["name": "Great Axe", "damage": "1d10", "weight": "Heavy", "rateOfFire": "1", "special": ""],
-        ["name": "Spear", "damage": "1d6", "weight": "Regular", "rateOfFire": "1", "special": "Can be thrown"],
-        ["name": "Short Bow", "damage": "1d6", "weight": "Regular", "rateOfFire": "1", "special": "Requires ammunition"],
-        ["name": "Long Bow", "damage": "1d8", "weight": "Regular", "rateOfFire": "1", "special": "Requires ammunition"],
-        ["name": "Crossbow", "damage": "1d8", "weight": "Regular", "rateOfFire": "1/2", "special": "Requires ammunition"],
-        ["name": "Sling", "damage": "1d4", "weight": "No size", "rateOfFire": "1", "special": "Requires ammunition"]
+        ["name": "Axe/Sword", "damage": "1d6+1", "weight": "Regular (1 slot)", "range": "20", "rateOfFire": "", "cost": "20", "special": ""],
+        ["name": "Club", "damage": "1d6-2", "weight": "Minor (2/slot)", "range": "", "rateOfFire": "", "cost": "-", "special": "Knock-out, improvised"],
+        ["name": "Crossbow", "damage": "1d6+1", "weight": "Heavy (2 slots)", "range": "70", "rateOfFire": "1/2", "cost": "50", "special": "Two handed"],
+        ["name": "Dagger", "damage": "1d6-2", "weight": "Minor (2/slot)", "range": "15", "rateOfFire": "1", "cost": "5", "special": ""],
+        ["name": "Darts", "damage": "1", "weight": "Minor (2/slot)", "range": "20", "rateOfFire": "3", "cost": "3", "special": "1–3 equal a minor item"],
+        ["name": "Flail", "damage": "1d6", "weight": "Regular (1 slot)", "range": "20", "rateOfFire": "", "cost": "20", "special": "Ignore shield DF"],
+        ["name": "Gr. sword/axe", "damage": "1d6+2", "weight": "Heavy (2 slots)", "range": "40", "rateOfFire": "", "cost": "40", "special": "Two handed"],
+        ["name": "Javelin", "damage": "1d6", "weight": "Minor (2/slot)", "range": "40", "rateOfFire": "1", "cost": "4", "special": "d6–2 damage in melee"],
+        ["name": "Longbow", "damage": "1d6", "weight": "Regular (1 slot)", "range": "70", "rateOfFire": "1", "cost": "25", "special": "Two handed"],
+        ["name": "Mace/Hammer", "damage": "1d6", "weight": "Regular (1 slot)", "range": "20", "rateOfFire": "", "cost": "20", "special": "+1 AV vs. metal armor"],
+        ["name": "Morning Star", "damage": "1d6", "weight": "Heavy (2 slots)", "range": "25", "rateOfFire": "", "cost": "25", "special": "As above, x3 crit dam."],
+        ["name": "Musket", "damage": "1d6+2", "weight": "Heavy (2 slots)", "range": "30", "rateOfFire": "1/4", "cost": "150", "special": "Two handed"],
+        ["name": "Pistol", "damage": "1d6+1", "weight": "Regular (1 slot)", "range": "20", "rateOfFire": "1/3", "cost": "100", "special": ""],
+        ["name": "Pole arms", "damage": "1d6", "weight": "Heavy (2 slots)", "range": "25", "rateOfFire": "", "cost": "25", "special": "Two handed, reach"],
+        ["name": "Quarterstaff", "damage": "1d6-1", "weight": "Regular (1 slot)", "range": "2", "rateOfFire": "", "cost": "2", "special": "Two handed, reach"],
+        ["name": "Scimitar", "damage": "1d6-1", "weight": "Regular (1 slot)", "range": "16", "rateOfFire": "", "cost": "16", "special": "+1 AV while riding"],
+        ["name": "Shortbow", "damage": "1d6-1", "weight": "Regular (1 slot)", "range": "50", "rateOfFire": "1", "cost": "20", "special": "Two handed, w. mount"],
+        ["name": "Shortsword", "damage": "1d6-1", "weight": "Minor (2/slot)", "range": "15", "rateOfFire": "", "cost": "15", "special": ""],
+        ["name": "Sling", "damage": "1d6-2", "weight": "No size (100/slot)", "range": "30", "rateOfFire": "1", "cost": "2", "special": "Use with stones"],
+        ["name": "Spear", "damage": "1d6", "weight": "Regular (1 slot)", "range": "2", "rateOfFire": "", "cost": "", "special": "Reach"],
+        ["name": "Thr. Knife/Axe", "damage": "1d6-2", "weight": "Minor (2/slot)", "range": "25", "rateOfFire": "1", "cost": "4", "special": "−1 AV in melee"],
+        ["name": "Unarmed", "damage": "d6-3", "weight": "", "range": "", "rateOfFire": "", "cost": "-", "special": "Knock-out, grapple"]
     ]
 }
