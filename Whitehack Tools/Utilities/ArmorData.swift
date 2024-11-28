98975d641b2d61abae64771319a59f69d2990892
zftcce czgqbs <alexanderstclair@zoho.com>
2024-11-28 13:12:18 -0800

    WORKING ON ARMOR pt 21

--- Whitehack Tools/Utilities/ArmorData.swift
+++ Whitehack Tools/Utilities/ArmorData.swift
@@ -13,10 +13,10 @@ struct ArmorData {
     static let armors: [ArmorItem] = [
         ArmorItem(name: "Shield", df: 1, weight: 1, special: "", isShield: true),
         ArmorItem(name: "Cloth", df: 1, weight: 1, special: "", isShield: false),
-        ArmorItem(name: "Leather", df: 2, weight: 1, special: "", isShield: false),
-        ArmorItem(name: "Hard leather", df: 3, weight: 2, special: "", isShield: false),
-        ArmorItem(name: "Chainmail", df: 4, weight: 3, special: "", isShield: false),
-        ArmorItem(name: "Splint mail", df: 5, weight: 3, special: "", isShield: false),
-        ArmorItem(name: "Full plate", df: 6, weight: 4, special: "", isShield: false)
+        ArmorItem(name: "Leather", df: 2, weight: 2, special: "", isShield: false),
+        ArmorItem(name: "Hard leather", df: 3, weight: 3, special: "", isShield: false),
+        ArmorItem(name: "Chainmail", df: 4, weight: 4, special: "", isShield: false),
+        ArmorItem(name: "Splint mail", df: 5, weight: 5, special: "", isShield: false),
+        ArmorItem(name: "Full plate", df: 6, weight: 6, special: "", isShield: false)
     ]
 }
