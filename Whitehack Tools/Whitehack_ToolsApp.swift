a5854a7cd641021348210295ab843f1e38317784
Alexander Bardelmeier <alexander@netartisancollective.com>
2024-12-21 23:53:26 -0800

    removed tabbing on macons

--- Whitehack Tools/Whitehack_ToolsApp.swift
+++ Whitehack Tools/Whitehack_ToolsApp.swift
@@ -6,6 +6,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
 }
 #endif
 
