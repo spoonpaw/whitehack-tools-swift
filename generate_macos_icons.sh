eaaebccd7d0653fdb21d9e0f8820e1d352c420dd
Alexander Bardelmeier <alexander@netartisancollective.com>
2024-12-29 18:40:03 -0800

    got macos icon to appear pt 3

--- generate_macos_icons.sh
+++ generate_macos_icons.sh
@@ -9,14 +9,11 @@ sizes=(16 32 128 256 512)
 
 for size in "${sizes[@]}"; do
     # Generate 1x version
-    sips -z $size $size "$ORIGINAL_ICON" --out "$ASSETS_DIR/icon_${size}x${size}.png"
+    sips -s format png -z $size $size "$ORIGINAL_ICON" --out "$ASSETS_DIR/icon_${size}x${size}.png"
     
     # Generate 2x version (including 512x512@2x)
     double_size=$((size * 2))
-    sips -z $double_size $double_size "$ORIGINAL_ICON" --out "$ASSETS_DIR/icon_${size}x${size}@2x.png"
+    sips -s format png -z $double_size $double_size "$ORIGINAL_ICON" --out "$ASSETS_DIR/icon_${size}x${size}@2x.png"
 done
-
-# Clean up any 64x64 icons if they exist
-rm -f "$ASSETS_DIR/icon_64x64.png" "$ASSETS_DIR/icon_64x64@2x.png"
 
 echo "Icon generation complete!"
